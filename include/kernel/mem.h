2cdb637aa63a41024846e7388576ada17343efa2
Dessera <dessera@qq.com>
2025-02-28 22:45:22 +0800

    page table

--- include/kernel/mem.h
+++ include/kernel/mem.h
@@ -5,3 +5,10 @@
 
 #define PAGE_SIZE 4096
 #define PAGE_TABLE_ADDR 0x100000 // 1MB
+#define INDEX_PAGE_TABLE_ADDR(index) ((index) * 0x1000 + PAGE_TABLE_ADDR)
+
+#define PDE_P(sign) (sign)
+#define PDE_RW(sign) (sign << 1)
+#define PDE_US(sign) (sign << 2)
+
+#define PDE_DESC(base, p, w, u) ((base) | PDE_P(p) | PDE_RW(w) | PDE_US(u))
--- include/kernel/screen.h
+++ include/kernel/screen.h
@@ -1,5 +1,6 @@
 #pragma once
 
 #define SCREEN_WIDTH 0xa0
+#define SCREEN_HEIGHT 0x28
 
 #define SCREEN_BUFFER_ADDR 0xb8000
